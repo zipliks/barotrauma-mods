f3d228a2b972565d0b6c4b491a0b5cdf6b3af998
zipliks <zipliks@gmail.com>
2025-06-15 23:19:11 +0300

    rename directories

--- oxygen-reloader/Lua/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-Config.VERSION = "1.0.2"
-Config.BUTTON = Keys.Q -- https://docs.monogame.net/api/Microsoft.Xna.Framework.Input.Keys.html
-Config.MIN_SWAP_CONDITION = 50 -- Minimal item condition to be swapped
-Config.MIN_SUITABLE_CONDITION = 90 -- Minimal item condition to be treated as variant
